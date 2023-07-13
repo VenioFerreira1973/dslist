afef84a0a1fcabe6a9e36c01a121c999df1f8462
VenioFerreira1973 <venioferreira@gmail.com>
2023-07-13 15:22:59 -0300

    Get by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 
@@ -24,5 +26,9 @@ public class GameController {
 		
 	}
 
+	@GetMapping("/{gameId}")
+	public GameDTO buscar(@PathVariable Long gameId ) {
+		return gameService.buscar(gameId);
+	}
 
 }
